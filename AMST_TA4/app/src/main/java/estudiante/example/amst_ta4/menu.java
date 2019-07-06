4b366f25ce0c0c89676c8077e13643fe680b6e8d
jimmygilces <jimmygilces@gmail.com>
2019-07-06 10:34:38 -0500

    SE AGREGAN COMENTARIOS A LA CLASE menu

--- AMST_TA4/app/src/main/java/estudiante/example/amst_ta4/menu.java
+++ AMST_TA4/app/src/main/java/estudiante/example/amst_ta4/menu.java
@@ -1,3 +1,8 @@
+/**
+ @author: GRUPO DE TRABAJO #1
+ LA PRESENTE CLASE REPRESENTA LA FUNCIONALIDAD A NIVEL LOGICO DEL MENU DE OPCIONES.
+ EL MENU CONTIENE DOS BOTONES, UNO PARA SALIR DE LA AOLICACION Y OTRO PARA REVISAR LOS SENSORES
+**/
 package estudiante.example.amst_ta4;
 
 import android.content.Intent;
@@ -8,18 +13,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class menu extends AppCompatActivity {
 
-    String token = "";
+    String token = ""; /*EL TOKEN PERMITE EL USO CORRECTO DE LA ACTIVIDAD*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        Intent login = getIntent();
-        this.token = (String)login.getExtras().get("token");
+        Intent login = getIntent(); /*INTENT PARA EXTRAER EL TOKEN GUARDADO POR LA ACTIVIDAD DE LOGIN*/
+        this.token = (String)login.getExtras().get("token"); /*EXTRACCION DEL TOKEN*/
     }
+
+    /**
+    METODO PARA SALIR DE LA ACTIVIDAD ACTUAL 
+    **/
     public void Salir(View v){
         this.finish();
         System.exit(0);
     }
+
+    /*METODO PARA IR A LA ACTIVIDAD RED_SENSORES*/
     public void revisarSensores(View v){
         Intent red_sensores = new Intent(getBaseContext(), red_sensores.class);
         red_sensores.putExtra("token", token);
